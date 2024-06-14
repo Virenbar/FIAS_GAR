9e2ffb2c3850ea2af45d18a0cba5fc0176074776
Virenbar <rib.artem@gmail.com>
2024-06-14 14:46:07 +0500

    feat: функция проверки наличия букв

---
+++ FIAS_GAR/dbo/Functions/SUF_HasLetter.sql
@@ -0,0 +1,12 @@
+﻿-- =============================================
+-- Author:		Artyom
+-- Create date:	14.05.2024
+-- Description:	Проверка наличия букв в строке
+-- =============================================
+CREATE FUNCTION [dbo].[SUF_HasLetter](
+	@String VARCHAR(500))
+RETURNS BIT
+AS
+BEGIN
+	RETURN IIF(@String LIKE '%[a-zA-Zа-яА-Я]%', 1, 0)
+END
