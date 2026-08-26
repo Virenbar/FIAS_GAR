52656a9dbc8f61878b61298f90624e726c44eb52
Virenbar <rib.artem@gmail.com>
2026-08-26 16:48:44 +0500

    fix: несколько параметров одного типа
    Параметры с несколькими значениями теперь будут пронумерованы.

--- FIAS_GAR/dbo/Stored Procedures/UP_ObjectParameters.sql
+++ FIAS_GAR/dbo/Stored Procedures/UP_ObjectParameters.sql
@@ -12,10 +12,20 @@ BEGIN
 	SET @ID = [dbo].[SUF_GUIDToID](@GUID)
 
 	SELECT
-		[P].[NAME]  [Name]
-	  , [P].[VALUE] [Value]
-	FROM
-		[V_Parameters] [P]
-	WHERE [P].[OBJECTID] = @ID
-	-- EXEC [UP_ObjectParameters] 'f31f9c11-a888-41c2-9cb2-9f86c486fecc'
+		  [Name] + IIF([Count] = 1, '', ' #' + CONVERT(VARCHAR(10), [Number])) [Name]
+		, [Value]                                                             
+	FROM (SELECT
+		  [NAME]                             [Name]
+		, ROW_NUMBER() OVER(PARTITION BY [NAME]
+		ORDER BY
+		[VALUE])                             [Number]
+		, [P].[VALUE]                        [Value]
+		, COUNT(*) OVER(PARTITION BY [NAME]) [Count]
+	FROM [V_Parameters] [P]
+	WHERE
+		[P].[OBJECTID] = @ID) [x]
+
+-- EXEC [UP_ObjectParameters] 'f31f9c11-a888-41c2-9cb2-9f86c486fecc'
+-- EXEC [UP_ObjectParameters] 'b5475007-a8ec-4a44-ae4e-b86b35921cbe'
+-- EXEC [UP_ObjectParameters] 'e592bad8-b6d2-46cb-be91-0a7356327d71'
 END
